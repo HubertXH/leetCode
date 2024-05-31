e8f319d15c3ecb6a5a99fb3ce1ff93f1132a2e2a
HubertXH <Hubert_XH@163.com>
2024-05-31 14:49:17 +0800

    Update UniquePaths.java
    MOD: 62-UniquePaths 增加解题思路解释

--- src/main/java/recursiondivide/UniquePaths.java
+++ src/main/java/recursiondivide/UniquePaths.java
@@ -41,6 +41,11 @@ public class UniquePaths {
 
     /**
      * DFS&DP
+     * 典型的动态规划问题。
+     * 求grid[m][n]的路径，能到达grid的路径只有两个，grid[m-1][n]和grid[m][n-1]
+     * 所以就需要求的到达grid[m-1][n]和grid[m][n-1]的路径分别为多少，即得到 grid[m][n] = grid[m-1][n] + grid[m][n-1];
+     * 因为只能向右或者向下移动所以，当m或者n为0的时候即grid[0][n]=1、grid[m][0]=1；
+     * 现在只需要循环遍历整个数组，将每个元素的路劲根据 grid[m][n] = grid[m-1][n] + grid[m][n-1] 计算出来即可。
      */
     public static int uniquePaths(int m, int n) {
         int[][] temps = new int[m][n];
